a80ab66f491ca2d262912607f3bb89e6c10ab18a
kpphiri <kphiri6@gatech.edu>
2019-03-24 12:24:26 -0400

    moved azure api to server.js

--- server/analyzer.js
+++ server/analyzer.js
@@ -1,33 +1,38 @@
-var exports = module.exports = {};
-exports.analyze = function() {
-  var myPythonScriptPath = './server/analyze-local-image.py';
-  var fs = require('fs');
+var myPythonScriptPath = './server/analyze-local-image.py';
+var fs = require('fs');
 
-  // Use python shell
-  const {PythonShell} = require("python-shell");
-  var pyshell = new PythonShell(myPythonScriptPath);
-  module.exports = {
-    analyze: function (dirname) {
-      pyshell.on('message', function (message) {
-          // received a message sent from the Python script (a simple "print" statement)
-          console.log(message);
-      });
+// Use python shell
+const {PythonShell} = require("python-shell");
+var pyshell = new PythonShell(myPythonScriptPath);
 
-      // end the input stream and allow the process to exit
-      pyshell.end(function (err) {
-          if (err){
-              throw err;
-          };
 
-          console.log('finished');
-      });
+// 
+// module.exports = {
+//   analyze: function (dirname) {
+//     console.log("STARINTGKSDFJFLS");
+//     var output = "";
+//      pyshell.on('message', function (message) {
+//         // received a message sent from the Python script (a simple "print" statement)
+//         console.log(message);
+//         output = message;
+//     });
+//
+//     // end the input stream and allow the process to exit
+//     pyshell.end(function (err) {
+//         if (err){
+//             throw err;
+//         };
+//         console.log('finished');
+//         return;
+//     });
+//
+//   }
+//
+// };
+
+function sleep(time) {
+    var stop = new Date().getTime();
+    while(new Date().getTime() < stop + time) {
+        ;
     }
-  };
-
-  function sleep(time) {
-      var stop = new Date().getTime();
-      while(new Date().getTime() < stop + time) {
-          ;
-      }
-  }
 }
